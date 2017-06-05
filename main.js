b660e25f71d0f2aee0a6b69f27ddea71d2704961
plupinska <patrycjalupinska@gmail.com>
2017-06-05 15:53:31 -0400

    Add router and scenes

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 import Expo from 'expo';
 import React from 'react';
+import { Router, Scene } from 'react-native-router-flux';
 import { StyleSheet, Text, View } from 'react-native';
 import LandingPage from './assets/components/landingPage';
 
 class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <LandingPage/>
-      </View>
+      <Router>
+        <Scene key="root">
+          <Scene key="landingPage" component={LandingPage} initial={true} />
+        </Scene>
+      </Router>
     );
   }
 }
