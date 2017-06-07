1fc8182f4be6ef655e90e2c59871eabfc7fc30fc
jestir1234 <jestir1234@gmail.com>
2017-06-06 22:16:05 -0400

    add changes

--- assets/components/Feed.js
+++ assets/components/Feed.js
@@ -87,11 +87,10 @@ class Feed extends Component {
             By {item.Source}
           </Text>
         </TouchableOpacity>
-        <View>
-          <Button
-            title={'Remove'}
-            onPress={() => this.removeItem(item)}
-            />
+        <View style={styles.trashWrapper}>
+          <TouchableOpacity onPress={() => this.removeItem(item)}>
+            <Image source={require('../images/trash.png')} />
+          </TouchableOpacity>
         </View>
       </View>
     )
@@ -227,6 +226,12 @@ const styles = StyleSheet.create({
    position: "absolute",
    right: 10,
    bottom: 5
+ },
+ trashWrapper: {
+   width: 100,
+   height: 50,
+   justifyContent: 'center',
+   alignItems: 'center',
  },
  chatBubble: {
    width: 130,
